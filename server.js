20a3ee61eb6a26f1115b7106a86d6acfa3329db7
papersnake <cctv5cn@gmail.com>
2016-05-13 11:51:13 +0800

    添加跨域支持

--- server.js
+++ server.js
@@ -23,12 +23,23 @@ let app = express();
 let orm = new Waterline();
 orm.loadCollection(Character);
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+
 app.set('port',process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(favicon(path.join(__dirname,'public','favicon.png')));
 app.use(express.static(path.join(__dirname,'public')));
+app.use(allowCrossDomain);
+
 
 
 app.get('/api/stats', (req,res,next) => {
